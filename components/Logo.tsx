d0bd945681491fb040de4d08497bdf1fc6558f23
Ishak Shekh <113578052+IshakShekh97@users.noreply.github.com>
2024-10-22 12:38:05 +0530

    Update Logo.tsx
    Changed Name from Budget Tracker to Budget Tracking

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -7,7 +7,7 @@ const Logo = () => {
     <Link href={"/"} className="flex items-center gap-2">
       <BanknoteIcon className="stroke h-11 w-11 stroke-orange-500 stroke-[1.5]" />
       <p className="bg-gradient-to-r from-amber-400  to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
-        Budget Tracker
+        Budget Tracking
       </p>
     </Link>
   );
@@ -18,7 +18,7 @@ export const LogoMobile = () => {
     <Link href={"/"} className="flex items-center gap-2">
       <BanknoteIcon className="stroke h-6 w-6 stroke-orange-500 stroke-[1.5]" />
       <p className="bg-gradient-to-r from-amber-400  to-orange-500 bg-clip-text max-sm:text-lg sm:text-3xl  font-bold leading-tight tracking-tighter text-transparent">
-        Budget Tracker
+        Budget Tracking
       </p>
     </Link>
   );
